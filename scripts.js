30c4563112a034847abbd63ce877b7e223386fb5
Andreas <formspiel@gmail.com>
2022-12-29 17:04:30 +0000

    Convert to tab see https://twitter.com/Rich_Harris/status/1541761871585464323

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
 const beforeUnloadListener = (event) => {
-    event.preventDefault();
-    return event.returnValue = "";
-  };
-  
-  const nameInput = document.querySelector("#emailAddress");
-  
-  nameInput.addEventListener("input", (event) => {
-    if (event.target.value !== "") {
-      addEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    } else {
-      removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    }
-  });
+	event.preventDefault();
+	return event.returnValue = "";
+};
 
-  const passwordInput = document.querySelector("#passwordInput");
-  
-  passwordInput.addEventListener("input", (event) => {
-    if (event.target.value !== "") {
-      addEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    } else {
-      removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    }
-  });
+const nameInput = document.querySelector("#emailAddress");
+
+nameInput.addEventListener("input", (event) => {
+	if (event.target.value !== "") {
+		addEventListener("beforeunload", beforeUnloadListener, {capture: true});
+	} else {
+		removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
+	}
+});
+
+const passwordInput = document.querySelector("#passwordInput");
+
+passwordInput.addEventListener("input", (event) => {
+	if (event.target.value !== "") {
+		addEventListener("beforeunload", beforeUnloadListener, {capture: true});
+	} else {
+		removeEventListener("beforeunload", beforeUnloadListener, {capture: true});
+	}
+});
